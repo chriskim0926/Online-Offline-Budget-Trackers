f2367f59920aeabc74b81292db9f85de81fba54f
chris kim <chriskim0926@gmail.com>
2021-01-16 03:51:06 -0500

    update server.js connection

--- server.js
+++ server.js
@@ -15,15 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
